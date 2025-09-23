1ece99ca4d6465cf02c7e14048cacef78ea4e141
zaynesmith <zayne.smith18@gmail.com>
2025-09-23 10:17:06 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "2rem",
+      fontFamily: "system-ui, sans-serif"
+    }}>
+      🚀 TravelingOvertimeJobs.com is Live!
+    </div>
+  );
+}
