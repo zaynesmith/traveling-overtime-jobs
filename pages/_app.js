82b4a09def65b76ee4e2a4e22dca0c09a3a4bc35
zaynesmith <zayne.smith18@gmail.com>
2025-09-26 15:29:43 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 // pages/_app.js
-import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Header from "../components/Header";
+import "../styles/globals.css"; // use a simple relative path
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
-      <Header />
       <Component {...pageProps} />
     </ClerkProvider>
   );
