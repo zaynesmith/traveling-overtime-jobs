1e92b314f680baaf7ef4ee3c4f7880d31011c4d1
zaynesmith <zayne.smith18@gmail.com>
2025-09-26 11:25:10 -0600

    Delete pages/sign-in/employer.js

--- pages/sign-in/employer.js
+++
@@ -1,9 +0,0 @@
-export default function EmployerSignIn() {
-  return (
-    <main style={{padding: '40px', fontFamily: 'system-ui'}}>
-      <h1>Employer Login</h1>
-      <p>Authentication coming next. For now, this is a placeholder.</p>
-      <p><a href="/">← Back to Home</a></p>
-    </main>
-  );
-}
