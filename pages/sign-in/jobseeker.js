d159425d63354cfa94f96fff53e0cc6e6bc0bc70
zaynesmith <zayne.smith18@gmail.com>
2025-09-26 11:25:21 -0600

    Delete pages/sign-in/jobseeker.js

--- pages/sign-in/jobseeker.js
+++
@@ -1,9 +0,0 @@
-export default function JobseekerSignIn() {
-  return (
-    <main style={{padding: '40px', fontFamily: 'system-ui'}}>
-      <h1>Jobseeker Login</h1>
-      <p>Authentication coming next. For now, this is a placeholder.</p>
-      <p><a href="/">← Back to Home</a></p>
-    </main>
-  );
-}
