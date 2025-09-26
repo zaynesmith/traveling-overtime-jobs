499aebb96f6ba6edbc24298f1a6ce70d0093d9f6
zaynesmith <zayne.smith18@gmail.com>
2025-09-26 11:02:45 -0600

    Create index.js

---
+++ pages/jobseeker/index.js
@@ -0,0 +1,7 @@
+export default function JobseekerLogin() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+      <h1>Jobseeker Login</h1>
+    </div>
+  );
+}
