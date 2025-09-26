767953700720907527431b874d9b61ac87ccfdac
zaynesmith <zayne.smith18@gmail.com>
2025-09-26 11:01:29 -0600

    Create index.js

---
+++ pages/employer/index.js
@@ -0,0 +1,7 @@
+export default function EmployerLogin() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+      <h1>Employer Login</h1>
+    </div>
+  );
+}
