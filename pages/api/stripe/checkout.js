aaae7db2251867b01627a4e26b5e1792cf4b38b1
zaynesmith <zayne.smith18@gmail.com>
2025-12-04 13:18:11 -0600

    Enable Stripe promotion codes for employer subscriptions

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -37,6 +37,7 @@ export default async function handler(req, res) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
+      allow_promotion_codes: true,
       line_items: [
         {
           price,
